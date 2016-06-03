4757d070ac378f5465d591807c4d02af26159ab3
noah <noah@haimi.com>
2016-06-03 18:10:29 +0800

    Signed-off-by: noah <noah@haimi.com>

--- workspace/pythonfirst/BeautifulSoup范例/BeautifulSoupTag.py
+++ workspace/pythonfirst/BeautifulSoup范例/BeautifulSoupTag.py
@@ -11,38 +11,37 @@ Created on 2016年6月2日
 import urllib.request
 from bs4 import BeautifulSoup
 from setuptools.compat import unicode
-# 'http://baike.baidu.com/view/21087.htm'
-# content = urllib.request.urlopen('http://baike.baidu.com/view/21087.htm').read()
-# html=BeautifulSoup(content,"html.parser")
-soup=BeautifulSoup('<b class="boldest">Extremely bold</b>',"html.parser")
 
+soup=BeautifulSoup('<b class="boldest">Extremely bold</b>',"html.parser")
 
 # 修改html部分
 
 
 # tag部分
-# 1:获取tag为b的tag
+
 tag=soup.b
 type(tag)
 tagtype=type(tag)
 print('tagtype     ',tagtype)
 # tagtype      <class 'bs4.element.Tag'>
+# 1:获取tag为b的tag
 
-# 2:修改tag里的class和id
 tag['class'] = 'verybold'
 tag['id'] = 1
 print('tag     ',tag)
 # tag      <b class="verybold" id="1">Extremely bold</b>
 print('获取定义过的tagclass     ',tag.get('class'))
 # 获取定义过的tagclass      verybold
+# 2:修改tag里的class和id
 
-# 3:删除class和id
+
 del tag['class']
 del tag['id']
 print('删除tagclass后的tag     ',tag)
+# 3:删除class和id
 # 删除tagclass后的tag      <b>Extremely bold</b>
 
-# 4:提取tag里面的字符串
+
 soup=BeautifulSoup('<b class="boldest">Extremely bold</b>',"html.parser")
 tag=soup.b
 tag.string
@@ -51,21 +50,25 @@ print(tag.string)
 type(tag.string)
 # <class 'bs4.element.NavigableString'>
 print(type(tag.string))
+# 4:提取tag里面的字符串
 
-# 转码  从 <class 'bs4.element.NavigableString'>变为 <class 'str'>
+
 unicode_string = unicode(tag.string)
 print(unicode_string)
 # u'Extremely bold'
 type(unicode_string)
 print(type(unicode_string))
 # <class 'str'>
+# 转码  从 <class 'bs4.element.NavigableString'>变为 <class 'str'>
 
-# 修改tag内的字符串
+
 soup=BeautifulSoup('<b class="boldest">Extremely bold</b>',"html.parser")
 tag=soup.b
-# tag.string.replace_with
+# 修改tag内的字符串
+
 tag.string.replace_with("No longer bold")
 print(tag)
+# tag.string.replace_with
 # <b class="boldest">No longer bold</b>
 
 
@@ -74,42 +77,47 @@ print(tag)
 
 # HTML5部分
 
-# 1:获取class，以列表形式输出
-# css_soup.p['class']
+
 css_soup = BeautifulSoup('<p class="body strikeout"></p>',"html5lib")
 print('html5获取多个class，结果列表     ',css_soup.p['class'])
+# 1:获取class，以列表形式输出
+# css_soup.p['class']
 # html5获取多个class，结果列表      ['body', 'strikeout']
 
 css_soup = BeautifulSoup('<p class="body"></p>',"html5lib")
 print('html5获取单个class，结果列表      ',css_soup.p['class'])
 # html5获取单个class，结果列表       ['body']
 
-# 2:获取id（只有一个）
-# id_soup.p['id']
+
 id_soup = BeautifulSoup('<p id="my id"></p>',"html5lib")
 print('获取id     ',id_soup.p['id'])
+# 2:获取id（只有一个）
+# id_soup.p['id']
 # 获取id      my id
 
-# 3:查看其中一个属性
-# rel_soup.a['rel']
+
 rel_soup = BeautifulSoup('<p>Back to the <a rel="index">homepage</a></p>',"html5lib")
 print('查看其中一个属性,如\'rel\'     ',rel_soup.a['rel'])
+# 3:查看其中一个属性
+# rel_soup.a['rel']
 # 查看其中一个属性,如'rel'      ['index']
 
-# 4:修改一个属性如[rel],重新查看
+
 rel_soup.a['rel'] = ['index', 'contents']
 print('修改后重新查看     ',rel_soup.p)
+# 4:修改一个属性如[rel],重新查看
 # 修改后重新查看      <p>Back to the <a rel="index contents">homepage</a></p>
 
 
-# 因为系统问题无法安装库   pip install lxml  Microsoft Visual C++ 10.0 is required (Unable to find vcvarsall.bat)
-# 解决办法:下载安装VS2010  http://pan.baidu.com/share/link?shareid=1609273194&uk=3255422755
-# 官网https://www.visualstudio.com/en-us/downloads/download-visual-studio-vs
+
 
 # xml_soup = BeautifulSoup('<p class="body strikeout"></p>', 'xml')
 # xml_soup.p['class']
 # print(xml_soup.p['class'])
 # u'body strikeout'
+# 因为系统问题无法安装库   pip install lxml  Microsoft Visual C++ 10.0 is required (Unable to find vcvarsall.bat)
+# 解决办法:下载安装VS2010  http://pan.baidu.com/share/link?shareid=1609273194&uk=3255422755
+# 官网https://www.visualstudio.com/en-us/downloads/download-visual-studio-vs
 
 
 
