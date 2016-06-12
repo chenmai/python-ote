b70ee7342dfcfcc72ed38efdb29939f6d5947cff
chenmai <chenmai98@qq.com>
2016-06-12 21:46:12 +0800

    Signed-off-by: chenmai <chenmai98@qq.com>

---
+++ workspace/pythonfirst/GUI/消息.py
@@ -0,0 +1,50 @@
+'''
+Created on 2016年6月11日
+
+@author: hasee
+'''
+import sys
+
+from PyQt5 import QtWidgets, QtCore
+
+# app = QtWidgets.QApplication(sys.argv)
+# 
+# widget = QtWidgets.QWidget()
+# 
+# widget.resize(360, 360)
+# 
+# widget.setWindowTitle("Hello, PyQt5!")
+# 
+# widget.show()
+# 
+# sys.exit(app.exec_())
+
+  
+  
+# #pyqt窗口必须在QApplication方法中使用  
+# app=QtWidgets.QApplication(sys.argv)  
+#   
+#   
+# label=QtWidgets.QLabel("<p style='color: red; margin-left: 20px'><b>hell world</b></p>")      #qt支持html标签，强大吧  
+# #有了实例，就需要用show()让他显示  
+# label.show()  
+#   
+#   
+# sys.exit(app.exec_())  
+
+
+#从PyQt库导入QtWidget通用窗口类  
+class mywindow(QtWidgets.QWidget):  
+#自己建一个mywindows类，以class开头，mywindows是自己的类名，  
+#（QtWidgets.QWidget）是继承QtWidgets.QWidget类方法，  
+    def __init__(self):  
+        super(mywindow,self).__init__()  
+  
+
+app = QtWidgets.QApplication(sys.argv)  
+windows = mywindow()  
+label=QtWidgets.QLabel(windows)     #在窗口中绑定label  
+label.setText("hello world")  
+  
+windows.show()  
+sys.exit(app.exec_())  
